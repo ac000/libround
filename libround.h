07bf360451b06ed8de7c4fba5ac4e8d6f0a09cb7
Andrew Clayton <andrew@digital-domain.net>
2017-01-29 02:05:45 +0000

    libround.h: Prevent C++ name mangling
    Use extern"C" { ... } around the function prototypes in the libround
    header file when it's being compiled as c++ to prevent function name
    mangling by the compiler.
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- libround.h
+++ libround.h
@@ -1,7 +1,9 @@
 /*
  * libround.h - Rounding algorithms API
  *
- * Copyright (C) 2010,2012	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 	2010, 2012, 2017
+ * 			Andrew Clayton <andrew@digital-domain.net>
+ *
  * Inspired by rounding-algorithms.hpp in the Boost C++ library.
  * Released under the GNU Lesser General Public License (LGPL) version 2.1
  * See COPYING
@@ -9,6 +11,10 @@
 
 #ifndef _LIBROUND_H_
 #define _LIBROUND_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 double lr_get_rounding_factor(int dp);
 double lr_ceil0(double value, double rf);
@@ -18,5 +24,9 @@ double lr_round_half_up0(double value, double rf);
 double lr_round_half_down(double value, double rf);
 double lr_round_half_down0(double value, double rf);
 double lr_round_half_even(double value, double rf);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* _LIBROUND_H_ */
