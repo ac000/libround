e523ba34a1607f63008c8c355fcf2eaa2d83b0df
Andrew Clayton <andrew@digital-domain.net>
2020-02-05 21:34:48 +0000

    libroundtest: Bring up to my current coding style
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- libroundtest.c
+++ libroundtest.c
@@ -4,7 +4,7 @@
 
 #include "libround.h"
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	double rf;
 
@@ -15,25 +15,25 @@ int main(int argc, char **argv)
 
 	printf("Rounding factor  : %f\n", rf);
 	printf("ceil             : %f --> %f\n",
-			atof(argv[1]), ceil(atof(argv[1]) * rf) / rf);
+	       atof(argv[1]), ceil(atof(argv[1]) * rf) / rf);
 	printf("ceil0            : %f --> %f\n",
-			atof(argv[1]), lr_ceil0(atof(argv[1]), rf));
+	       atof(argv[1]), lr_ceil0(atof(argv[1]), rf));
 	printf("floor            : %f --> %f\n",
-			atof(argv[1]), floor(atof(argv[1]) * rf) / rf);
+	       atof(argv[1]), floor(atof(argv[1]) * rf) / rf);
 	printf("floor0           : %f --> %f\n",
-			atof(argv[1]), lr_floor0(atof(argv[1]), rf));
+	       atof(argv[1]), lr_floor0(atof(argv[1]), rf));
 	printf("round            : %f --> %f\n",
-			atof(argv[1]), round(atof(argv[1]) * rf) / rf);
+	       atof(argv[1]), round(atof(argv[1]) * rf) / rf);
 	printf("round_half_up    : %f --> %f\n",
-			atof(argv[1]), lr_round_half_up(atof(argv[1]), rf));
+	       atof(argv[1]), lr_round_half_up(atof(argv[1]), rf));
 	printf("round_half_up0   : %f --> %f\n",
-			atof(argv[1]), lr_round_half_up0(atof(argv[1]), rf));
+	       atof(argv[1]), lr_round_half_up0(atof(argv[1]), rf));
 	printf("round_half_down  : %f --> %f\n",
-			atof(argv[1]), lr_round_half_down(atof(argv[1]), rf));
+	       atof(argv[1]), lr_round_half_down(atof(argv[1]), rf));
 	printf("round_half_down0 : %f --> %f\n",
-			atof(argv[1]), lr_round_half_down0(atof(argv[1]), rf));
+	       atof(argv[1]), lr_round_half_down0(atof(argv[1]), rf));
 	printf("round_half_even  : %f --> %f\n",
-			atof(argv[1]), lr_round_half_even(atof(argv[1]), rf));
+	       atof(argv[1]), lr_round_half_even(atof(argv[1]), rf));
 
 	exit(EXIT_SUCCESS);
 }
